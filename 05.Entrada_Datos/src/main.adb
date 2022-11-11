b3a006b3088148c777013acd6866ead515563fc8
Menéndez Fernández <bmenendez@indra.es>
2022-11-11 14:05:06 +0100

    :)

--- 05.Entrada_Datos/src/main.adb
+++ 05.Entrada_Datos/src/main.adb
@@ -1,8 +1,38 @@
-with ada.Text_IO; use ada.text_io;
+with Ada.Text_IO; use Ada.Text_IO;
 
 procedure Main is
+   --Nombre : String := Get_Line; No puedo preguntar antes
+   --Va a preguntarle un string al usuario hasta que no sea vacio
+   function Get_Non_Empty_Line
+     (Prompt:String := "Ingrese un texto no vacio";
+     Error:String := "Ha Ingresado un texto vacio, vuelva a intentarlo") return String is
+   begin
+      Put_Line(Prompt);
+      Lectura_Datos : loop
+         declare
+            Texto : String := Get_Line;
+         begin
+            if (Texto'Length>0) then
+               return Texto;
+            end if;
+            Put_Line(Error);
+         end;
+      end loop Lectura_Datos;
+   end Get_Non_Empty_Line;
 
+   function Try_Convert_Integer
+     (Texto:in String;
+      Texto_Como_Entero:out Integer) return Boolean is
+   begin
+      Texto_Como_Entero := Integer'Value(Texto);
+      return True;
+   exception
+      --when Constraint_Error => return False;
+      when others => return False;
+   end;
 
+   --Va a preguntar un valor hasta que el usuario ingrese un entero
+   --Usar la funcion anterior Try_Convert_Integer
    function Get_Integer
      (Prompt:String := "Ingresa un numero";
       Error:String := "No ha ingresado un numero, vuelva a intentarlo") return Integer is
@@ -15,109 +45,47 @@ procedure Main is
       return Result;
    end Get_Integer;
 
-   function Get_Integer_Between(Min:Integer;
-                                Max:Integer;
-                                Prompt:String:="Ingrese un número";
-                                Empty_Error:String:="Texto vacío, vuelva a intentarlo";
-                                Underflow_Error:String:="Numero muy pequeño";
-                                Overflow_Error:String:="Numero muy grande") return Integer is
-      Result : Integer;
-      begin
-         loop
-
-            Result := Get_Integer(Prompt, Empty_Error);
-            if Result > Max then
-               put_line(Overflow_Error);
-            elsif Result < Min then
-               put_line(Underflow_Error);
-            else
-               return Result;
-            end if;
-
-         end loop;
-
-
-      end Get_Integer_Between;
-
-
-
-   function Try_Convert_Integer(Texto : in String;
-                               Texto_Int : out Integer) return Boolean is
-      --  Coge un texto y
+   function Get_Integer_Between
+     (Min:Integer;
+      Max:Integer;
+      Prompt:String := "Ingrese un numero";
+      Empty_Error:String := "Texto Vacio, Vuelva a Intentar";
+      Underflow_Error:String := "Numero muy chico";
+      Overflow_Error:String := "Numero muy grande") return Integer is
+     Result : Integer;
    begin
-      Texto_Int := Integer'Value(Texto);
-      return true;
-   exception
-         when others => return False;
-
-   end Try_Convert_Integer;
-
-   function Get_Non_Empty_Line(Prompt : String := "Ingrese un texto no vacío";
-                               Error : string := "No has introducido texto") return string is
-
-   begin
-
-      put_line(Error);
-      put_line(Prompt);
-
-      declare
-
-         cadena : string := Get_Line;
-
-
-      begin
-         loop
-            put_line(Error);
-            put_line(Prompt);
-            cadena := Get_Line;
-            exit when (cadena'length > 0);
-         end loop;
-
-         return cadena;
-
-         end;
-
-   end Get_Non_Empty_Line;
-
-begin
-   --  Insert code here.
-
-   declare
-
-      numero : Integer;
-
-   begin
-
-      numero := Get_Integer ("Ingrese su edad");
-
+      loop
+         Result := Get_Integer(Prompt, Empty_Error);
+         if Result > Max then
+           Put_Line(Overflow_Error);
+         elsif Result<Min then
+           Put_Line(Underflow_Error);
+         else
+            return Result;
+         end if;
+      end loop;
    end;
 
 
-
-
-   --_line("Ingrese su nombre:");
-   --  put_line("Ingrese su edad: ");
-   --  declare
-   --     Edad : Integer := 0;
-   --  begin
-   --     if Try_Convert_Integer(Get_Line, Edad) then
-   --            Put_Line("Tienes " & Edad'Image);
-   --     else
-   --            Put_Line("No ingresaste un número");
-   --     end if;
-   --  end;
-
-
-   --  declare
-   --     Nombre : String := Get_Line;  --  Declaración implícita
-   --  begin
-   --
-   --     if Nombre'length = 0 then
-   --        Nombre := Get_Non_Empty_Line;
-   --
-   --     End if;
-   --     put_line("Hola " & Nombre);
-   --  end;
-
-   null;
+begin
+   --Put_Line("Ingrese Su Nombre");
+   declare
+      Nombre : String := Get_Non_Empty_Line("Ingrese Su Nombre"); --Declaracion de String en forma Implicita
+      Edad : Integer := 0;
+      --Nombre : String(1..100); --Explicita
+   begin
+      --Put_Line("Hola " & Nombre);
+      --Put_Line("Ingrese Su Edad");
+      Edad := Get_Integer("Ingrese Su Edad");
+      Put_Line("Hola " & Nombre & " de " & Edad'Image & " años");
+      --  declare
+      --     Edad : Integer := 0;
+      --  begin
+      --     if (Try_Convert_Integer(Get_Line, Edad)) then
+      --        Put_Line("Tienes " & Edad'Image);
+      --     else
+      --        Put_Line("No Ingresaste un numero");
+      --     end if;
+      --  end;
+   end;
 end Main;
