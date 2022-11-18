f748d13d949f11dcdd84f09bcc9dddeba2dcb8ec
bmenenefer <bmenendez@indra.es>
2022-11-18 11:42:55 +0100

    18.11.2022

--- 10.Genericos/src/generic_io.adb
+++ 10.Genericos/src/generic_io.adb
@@ -9,6 +9,19 @@ package body Generic_Io is
    exception
          when others => return False;
    end Try_Convert;
-     
+   
+   function Get_Discrete(Prompt : String := "Ingresa un número.";
+                         Error : String := "No ha ingresado un numero, vuelva a intentarlo") return Tipo is
+      
+   Temp : Tipo;
+   begin
+      Put_Line(Prompt);
+      while (not(Try_Convert(Get_Line, Temp))) loop
+         Put_Line(Error);
+      end loop;
+      return Result;
+      
+   end Get_Discrete;
+   
 
 end Generic_Io;
